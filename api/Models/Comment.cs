98e63a18a671f22828ccf2a1323db3045cc530bf
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-07-02 12:57:17 +0300

    comment and stock

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.linq;
-using System.threading.Tasks;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace api.Models
 {
     public class Comment
     {
-        
+        public int? StockId { get; set; }
     }
 }
--- api/Models/Stocks.cs
+++ api/Models/Stocks.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +11,14 @@ namespace api.Models
         public int Id { get; set; }
         public string Symbol { get; set; }=string.Empty;
         public string CompanyName { get; set; }=string.Empty;
-        [column(TypeName = "decimal(18,2)")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Purchase { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+        public String Industry { get; set; }=string.Empty;
+        public long MarketCap { get; set; }
+        public List<Comment> Comments { get; set; }=new List<Comment>();
+
+    
     }
 }
